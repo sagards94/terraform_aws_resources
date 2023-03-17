a0fc5af8d3f54c7b532c7d8c51f070ad78c9b447
sagards94 <125671685+sagards94@users.noreply.github.com>
2023-03-17 20:55:12 +0530

    Add files via upload

---
+++ modules/create_ec2/output.tf
@@ -0,0 +1,8 @@
+output "ec2_public_ip_address" {
+    value = aws_instance.my_ec2_instance.public_ip
+}
+
+output "ec2_private_ip_address" {
+    value = aws_instance.my_ec2_instance.private_ip
+}
+
